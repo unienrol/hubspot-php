847c48f7287667dffe3d95e027cf0df2a0a740fb
justin <justin.ang@unienrol.com>
2025-09-18 11:00:01 +0800

    chore: test

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -109,7 +109,9 @@ class Client
             if (false === $this->wrapResponse) {
                 return $this->client->request($method, $url, $options);
             }
+            if ($url === 'https://api.hubapi.com/crm/v3/objects/deals/search'){
             dd($options);
+        }
             return new Response($this->client->request($method, $url, $options));
         } catch (ServerException $e) {
             if (false === $this->wrapResponse) {
