dbdc8d9e9958e66ab16b387dcaeef9545970a863
justin <justin.ang@unienrol.com>
2025-09-18 11:26:25 +0800

    chore: test

--- src/Endpoints/Deals.php
+++ src/Endpoints/Deals.php
@@ -330,10 +330,10 @@ class Deals extends Endpoint
                 ]
             ],
             "sorts" => [
-                {
+                [
                     "propertyName": "createdate",
                     "direction": "DESCENDING"
-                }
+                ]
             ],
             "limit" => 1,
             "properties" => ["dealname", "createdAt", "amount", "contact_number"]
